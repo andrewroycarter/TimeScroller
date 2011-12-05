a48b2428e446ff48a0810355204fef191502decb
Andrew Carter <andrew.r.carter@gmail.com>
2011-12-04 21:27:14 -0500

    Initial Commit

---
+++ TimerScroller/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TimerScroller
+//
+//  Created by Andrew Carter on 12/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimerScroller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimerScroller
+//
+//  Created by Andrew Carter on 12/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
